318244b282e132eb412ddc97f003341a96eaae6c
rachel <rachel@checkpoint.com>
2020-07-23 15:53:14 +0300

    better zip code

--- src/utils/zipBuildproduct.js
+++ src/utils/zipBuildproduct.js
@@ -1,34 +1,13 @@
-var fs = require('fs');
-var JSZip = require('jszip');
-const { file } = require('jszip');
+const zipFolder = require('zip-a-folder');
 
-var zip = new JSZip();
+class ZipAFolder {
+  static main() {
+    zipFolder.zipFolder('build', 'build.zip', function(err) {
+      if (err) {
+        console.debug('Something went wrong!', err);
+      }
+    });
+  }
+}
 
-//go recursivly over
-const walkSync = function(dir, filelist) {
-  const files = fs.readdirSync(dir);
-  filelist = filelist || [];
-  files.forEach(function(file) {
-    if (fs.statSync(dir + '/' + file).isDirectory()) {
-      zip.folder(dir + '/' + file);
-      filelist = walkSync(dir + '/' + file, filelist);
-    } else {
-      filelist.push(dir + '/' + file);
-      zip.file(dir + '/' + file, file);
-    }
-  });
-  return filelist;
-};
-
-let allFiles = walkSync('build');
-
-console.log('ziped files', JSON.stringify(allFiles));
-
-zip
-  .generateNodeStream({ type: 'nodebuffer', streamFiles: true })
-  .pipe(fs.createWriteStream('build.zip'))
-  .on('finish', function() {
-    // JSZip generates a readable stream with a "end" event,
-    // but is piped here in a writable stream which emits a "finish" event.
-    console.log('build.zip written.');
-  });
+ZipAFolder.main();
